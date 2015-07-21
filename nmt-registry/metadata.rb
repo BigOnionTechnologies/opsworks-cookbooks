3d8699d196fefb556d87a334bf4cd207d54a7118
Brian Cipollone <brian@bigoniontech.com>
2015-07-21 14:08:53 -0500

    nmt

---
+++ nmt-registry/metadata.rb
@@ -0,0 +1,7 @@
+name        "nmt-registry"
+description 'Cookbook for MongoDB Berkshelf based install'
+maintainer  "BigOnionTechnologies"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
